51babc2f9f5432acdd146536dc9d2edc59317f80
deepak <dkp041297@gmail.com>
2023-10-30 20:46:46 +0530

    quiz Score

---
+++ quiz/src/QuizScore.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const QuizScore = () => {
+  return (
+    <div>
+        <h1>Your Score  <span></span> </h1>
+        <h1>Total quiz Marks is 18</h1>
+    </div>
+  )
+}
+
+export default QuizScore
